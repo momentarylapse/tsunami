aac834e60790707b0c65aaf391c65d63fcaa4836
Michael Ankele <michi@lupina.de>
2015-07-22 21:15:25 +0200

    silent Progress without TsunamiWindow

--- src/View/Helper/Progress.cpp
+++ src/View/Helper/Progress.cpp
@@ -12,12 +12,15 @@ const string Progress::MESSAGE_CANCEL = "Cancel";
 Progress::Progress(const string &str, HuiWindow *parent) :
 	Observable("Progress")
 {
-	dlg = HuiCreateResourceDialog("progress_dialog", parent);
-	dlg->setString("progress_bar", str);
-	dlg->setFloat("progress_bar", 0);
-	dlg->show();
-	dlg->eventS("hui:close", &HuiFuncIgnore);
-	HuiDoSingleMainLoop();
+	dlg = NULL;
+	if (parent){
+		dlg = HuiCreateResourceDialog("progress_dialog", parent);
+		dlg->setString("progress_bar", str);
+		dlg->setFloat("progress_bar", 0);
+		dlg->show();
+		dlg->eventS("hui:close", &HuiFuncIgnore);
+		HuiDoSingleMainLoop();
+	}
 	cancelled = false;
 }
 
@@ -29,22 +32,27 @@ Progress::Progress() :
 
 Progress::~Progress()
 {
-	delete(dlg);
+	if (dlg)
+		delete(dlg);
 }
 
 
 void Progress::set(const string &str, float progress)
 {
-	dlg->setString("progress_bar", str);
-	dlg->setFloat("progress_bar", progress);
-	HuiDoSingleMainLoop();
+	if (dlg){
+		dlg->setString("progress_bar", str);
+		dlg->setFloat("progress_bar", progress);
+		HuiDoSingleMainLoop();
+	}
 }
 
 
 void Progress::set(float progress)
 {
-	dlg->setFloat("progress_bar", progress);
-	HuiDoSingleMainLoop();
+	if (dlg){
+		dlg->setFloat("progress_bar", progress);
+		HuiDoSingleMainLoop();
+	}
 }
 
 void Progress::cancel()
@@ -63,13 +71,15 @@ bool Progress::isCancelled()
 ProgressCancelable::ProgressCancelable(const string &str, HuiWindow *parent) :
 	Progress()
 {
-	dlg = HuiCreateResourceDialog("progress_cancelable_dialog", parent);
-	dlg->setString("progress_bar", str);
-	dlg->setFloat("progress_bar", 0);
-	dlg->show();
-	dlg->event("hui:close", this, &Progress::cancel);
-	dlg->event("cancel", this, &Progress::cancel);
-	HuiDoSingleMainLoop();
+	if (parent){
+		dlg = HuiCreateResourceDialog("progress_cancelable_dialog", parent);
+		dlg->setString("progress_bar", str);
+		dlg->setFloat("progress_bar", 0);
+		dlg->show();
+		dlg->event("hui:close", this, &Progress::cancel);
+		dlg->event("cancel", this, &Progress::cancel);
+		HuiDoSingleMainLoop();
+	}
 }
 
 ProgressCancelable::~ProgressCancelable()

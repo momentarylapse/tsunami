4331fd0fde5a24a70d8d8e3492ee5143c49e1cfa
Michael Ankele <michi@lupina.de>
2015-12-10 13:53:08 +0100

    removed deprecated nami parser code

--- src/Storage/Format/FormatNami.h
+++ src/Storage/Format/FormatNami.h
@@ -22,27 +22,7 @@ public:
 	virtual void loadSong(StorageOperationData *od);
 	virtual void saveSong(StorageOperationData *od);
 
-	Array<int> ChunkPos;
 	void make_consistent(Song *s);
-
-	void BeginChunk(const string &name);
-	void EndChunk();
-	void WriteTag(Tag *t);
-	void WriteEffect(Effect *e);
-	void WriteBufferBox(BufferBox *b);
-	void WriteSample(Sample *s);
-	void WriteSampleRef(SampleRef *s);
-	void WriteBar(BarPattern &b);
-	void WriteMarker(TrackMarker &m);
-	void WriteMidiEvent(MidiEvent &e);
-	void WriteMidiEffect(MidiEffect *e);
-	void WriteMidi(MidiNoteData &m);
-	void WriteSynth(Synthesizer *s);
-	void WriteTuning(Array<int> &tuning);
-	void WriteTrackLevel(TrackLevel *l, int level_no);
-	void WriteTrack(Track *t);
-	void WriteLevelName();
-	void WriteFormat();
 
 	File *f;
 	Song *song;

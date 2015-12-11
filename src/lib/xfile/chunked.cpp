e30dd6ab4ea331bb60065bc6c0c617c3ed5a2276
Michael Ankele <michi@lupina.de>
2015-12-11 15:36:36 +0100

    lib/xfile/chunked

---
+++ src/lib/xfile/chunked.cpp
@@ -0,0 +1,265 @@
+/*
+ * chunked.cpp
+ *
+ *  Created on: Dec 11, 2015
+ *      Author: ankele
+ */
+
+#include "../base/base.h"
+#include "../file/file.h"
+#include "chunked.h"
+
+void strip(string &s)
+{
+	while((s.num > 0) and (s.back() == ' '))
+		s.resize(s.num - 1);
+}
+
+ChunkedFileParser::ChunkedFileParser(int _header_name_size)
+{
+	header_name_size = _header_name_size;
+	base = NULL;
+}
+
+ChunkedFileParser::Context::Context()
+{
+	f = NULL;
+}
+
+void ChunkedFileParser::Context::pop()
+{
+	layers.pop();
+}
+
+void ChunkedFileParser::Context::push(const string &name, int pos, int size)
+{
+	layers.add(ChunkedFileParser::Context::Layer(name, pos, size));
+}
+
+int ChunkedFileParser::Context::end()
+{
+	if (layers.num > 0)
+		return layers.back().pos0 + layers.back().size;
+	return 2000000000;
+}
+
+string ChunkedFileParser::Context::str()
+{
+	string s;
+	foreach(Layer &l, layers){
+		if (s.num > 0)
+			s += "/";
+		s += l.name;
+	}
+	return s;
+}
+
+FileChunkBasic::FileChunkBasic(const string &_name)
+{
+	name = _name;
+	root = NULL;
+	context = NULL;
+}
+FileChunkBasic::~FileChunkBasic()
+{
+	foreach(FileChunkBasic *c, children)
+		delete(c);
+}
+void FileChunkBasic::create()
+{
+	throw string("no data creation... " + name);
+}
+void FileChunkBasic::notify()
+{
+	if (root)
+		root->on_notify();
+}
+void FileChunkBasic::info(const string &message)
+{
+	if (root)
+		root->on_info(message);
+}
+void FileChunkBasic::warn(const string &message)
+{
+	if (root)
+		root->on_warn(message);
+}
+void FileChunkBasic::error(const string &message)
+{
+	if (root)
+		root->on_error(message);
+}
+
+void FileChunkBasic::add_child(FileChunkBasic *c)
+{
+	children.add(c);
+}
+void FileChunkBasic::set_root(ChunkedFileParser *r)
+{
+	root = r;
+	context = &r->context;
+	foreach(FileChunkBasic *c, children)
+		c->set_root(r);
+}
+
+FileChunkBasic *FileChunkBasic::get_sub(const string &name)
+{
+	foreach(FileChunkBasic *c, children)
+		if (c->name == name){
+			c->context = context;
+			c->set_parent(get());
+			return c;
+		}
+	throw string("no sub... " + name + " in " + context->str());
+}
+
+void FileChunkBasic::write_sub(const string &name, void *p)
+{
+	FileChunkBasic *c = get_sub(name);
+	c->set(p);
+	c->write_complete();
+}
+
+void FileChunkBasic::write_sub_parray(const string &name, DynamicArray &a)
+{
+	FileChunkBasic *c = get_sub(name);
+	Array<void*> *pa = (Array<void*>*)&a;
+	for (int i=0; i<pa->num; i++){
+		c->set((*pa)[i]);
+		c->write_complete();
+	}
+}
+void FileChunkBasic::write_sub_array(const string &name, DynamicArray &a)
+{
+	FileChunkBasic *c = get_sub(name);
+	for (int i=0; i<a.num; i++){
+		c->set((char*)a.data + a.element_size * i);
+		c->write_complete();
+	}
+}
+
+void FileChunkBasic::write_begin_chunk(File *f)
+{
+	string s = name + "        ";
+	f->WriteBuffer(s.data, root->header_name_size);
+	f->WriteInt(0); // temporary
+	context->push(name, context->f->GetPos(), 0);
+}
+
+void FileChunkBasic::write_end_chunk(File *f)
+{
+	int pos = f->GetPos();
+	int chunk_pos = context->layers.back().pos0;
+	f->SetPos(chunk_pos - 4, true);
+	f->WriteInt(pos - chunk_pos);
+	f->SetPos(pos, true);
+	context->pop();
+}
+void FileChunkBasic::write_complete()
+{
+	File *f = context->f;
+	write_begin_chunk(f);
+	write_contents();
+	write_end_chunk(f);
+}
+void FileChunkBasic::write_contents()
+{
+	File *f = context->f;
+	write(f);
+	write_subs();
+}
+
+string FileChunkBasic::read_header()
+{
+	string cname;
+	cname.resize(root->header_name_size);
+	context->f->ReadBuffer(cname.data, cname.num);
+	strip(cname);
+	int size = context->f->ReadInt();
+	int pos0 = context->f->GetPos();
+
+	if (size < 0)
+		throw string("chunk with negative size found");
+	if (pos0 + size > context->end())
+		throw string("inner chunk is larger than its parent");
+
+	context->push(cname, pos0, size);
+	return cname;
+}
+
+
+void FileChunkBasic::read_contents()
+{
+	File *f = context->f;
+
+	read(f);
+
+	// read nested chunks
+	while (f->GetPos() < context->end()){
+
+		string cname = read_header();
+
+		bool ok = false;
+		foreach(FileChunkBasic *c, children)
+			if (c->name == cname){
+				c->set_parent(get());
+				c->create();
+				c->context = context;
+				c->read_contents();
+				ok = true;
+				break;
+			}
+		if (!ok){
+			if (root)
+				root->on_unhandled();
+			//throw string("no sub handler: " + context->str());
+		}
+
+		f->SetPos(context->end(), true);
+		context->pop();
+	}
+}
+
+
+ChunkedFileParser::~ChunkedFileParser()
+{
+	delete base;
+}
+
+bool ChunkedFileParser::read(const string &filename, void *p)
+{
+	File *f = FileOpen(filename);
+	f->SetBinaryMode(true);
+	context.f = f;
+	//context.push(Context::Layer(name, 0, f->GetSize()));
+
+	base->set_root(this);
+	base->set(p);
+	string cname = base->read_header();
+	if (cname != base->name)
+		throw string("wrong base chunk: ") + cname + " (" + base->name + " expected)";
+	base->read_contents();
+
+	delete(f);
+
+	return true;
+}
+
+bool ChunkedFileParser::write(const string &filename, void *p)
+{
+	File *f = FileCreate(filename);
+	f->SetBinaryMode(true);
+	context.f = f;
+	//context->push(Context::Layer(name, 0, 0));
+
+	base->set_root(this);
+	base->set(p);
+	base->write_complete();
+
+	delete(f);
+
+	return true;
+}
+
+
+
---
+++ src/lib/xfile/chunked.h
@@ -0,0 +1,127 @@
+/*
+ * chunked.h
+ *
+ *  Created on: Dec 11, 2015
+ *      Author: ankele
+ */
+
+#ifndef SRC_LIB_XFILE_CHUNKED_H_
+#define SRC_LIB_XFILE_CHUNKED_H_
+
+
+class FileChunkBasic;
+
+class ChunkedFileParser
+{
+public:
+	ChunkedFileParser(int _header_name_size);
+	virtual ~ChunkedFileParser();
+
+	bool read(const string &filename, void *p);
+	bool write(const string &filename, void *p);
+	void set_base(FileChunkBasic *base);
+
+	virtual void on_notify(){};
+	virtual void on_unhandled(){};
+	virtual void on_error(const string &message){}
+	virtual void on_warn(const string &message){}
+	virtual void on_info(const string &message){}
+
+	FileChunkBasic *base;
+	int header_name_size;
+
+	struct Context
+	{
+		struct Layer
+		{
+			string name;
+			int pos0, size;
+			Layer(){}
+			Layer(const string &_name, int _pos, int _size)
+			{
+				name = _name;
+				pos0 = _pos;
+				size = _size;
+			}
+		};
+		Context();
+		void pop();
+		void push(const string &name, int pos, int size);
+		int end();
+		string str();
+
+		File *f;
+		Array<Layer> layers;
+	};
+	Context context;
+
+};
+
+class FileChunkBasic
+{
+public:
+	FileChunkBasic(const string &_name);
+	virtual ~FileChunkBasic();
+	virtual void write(File *f) = 0;
+	virtual void read(File *f) = 0;
+	virtual void create();
+	string name;
+
+	void notify();
+	void info(const string &message);
+	void warn(const string &message);
+	void error(const string &message);
+	void add_child(FileChunkBasic *c);
+	void set_root(ChunkedFileParser *r);
+
+	virtual void set(void *t){};
+	virtual void set_parent(void *t){};
+	virtual void *get(){ return 0; }
+
+	ChunkedFileParser::Context *context;
+	Array<FileChunkBasic*> children;
+	ChunkedFileParser *root;
+
+	virtual void write_subs(){}
+
+	FileChunkBasic *get_sub(const string &name);
+
+	void write_sub(const string &name, void *p);
+	void write_sub_parray(const string &name, DynamicArray &a);
+	void write_sub_array(const string &name, DynamicArray &a);
+	void write_begin_chunk(File *f);
+	void write_end_chunk(File *f);
+	void write_complete();
+	void write_contents();
+	string read_header();
+	void read_contents();
+};
+
+
+
+template<class PT, class T>
+class FileChunk : public FileChunkBasic
+{
+public:
+	FileChunk(const string &name) : FileChunkBasic(name){ me = NULL; parent = NULL; }
+	virtual void *get()
+	{
+		return me;
+	}
+	virtual void set(void *_me)
+	{
+		me = (T*)_me;
+	}
+	virtual void set_parent(void *_parent)
+	{
+		parent = (PT*)_parent;
+	}
+	T *me;
+	PT *parent;
+};
+
+
+
+
+
+#endif /* SRC_LIB_XFILE_CHUNKED_H_ */

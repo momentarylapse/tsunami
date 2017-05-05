71665f66cd182e2d44237a13a1a9f622e1f4111b
Michael Ankele <michi@lupina.de>
2017-05-05 14:51:07 +0200

    more drum "classical" notation

--- src/Midi/Clef.cpp
+++ src/Midi/Clef.cpp
@@ -15,6 +15,49 @@ const Clef Clef::BASS(Clef::TYPE_BASS, "𝄢", 3*7-3); // \uD834\uDD22    "\u1d1
 const Clef Clef::BASS_8(Clef::TYPE_BASS_8, "𝄤", 2*7-3); // \uD834\uDD24   "\u1d124";
 const Clef Clef::DRUMS(Clef::TYPE_DRUMS, "𝄥", 0);
 
+struct DrumClefPosition
+{
+	int pitch, clef_pos, modifier;
+};
+const int NUM_DRUM_CLEF_POSITIONS = 19;
+const DrumClefPosition DrumClefPositions[NUM_DRUM_CLEF_POSITIONS] =
+{
+	// drums
+	{DRUM_PITCH_BASS, 1, MODIFIER_NONE},
+	{DRUM_PITCH_BASS_ACCOUSTIC, 1, MODIFIER_NONE},
+	{DRUM_PITCH_TOM_FLOOR_LOW, 2, MODIFIER_NONE},
+	{DRUM_PITCH_TOM_FLOOR_HI, 3, MODIFIER_NONE},
+	{DRUM_PITCH_TOM_LOW, 4, MODIFIER_NONE},
+	{DRUM_PITCH_SNARE, 5, MODIFIER_NONE},
+	{DRUM_PITCH_SNARE_ELECTRONIC, 5, MODIFIER_NONE},
+	{DRUM_PITCH_TOM_LOW_MID, 6, MODIFIER_NONE},
+	{DRUM_PITCH_TOM_HI_MID, 7, MODIFIER_NONE},
+	{DRUM_PITCH_TOM_HI, 8, MODIFIER_NONE},
+
+	// cymbals
+	{DRUM_PITCH_HIHAT_PEDAL, -1, MODIFIER_SHARP},
+	{DRUM_PITCH_HIHAT_OPEN, 7, MODIFIER_SHARP},
+	{DRUM_PITCH_HIHAT_CLOSED, 7, MODIFIER_FLAT},
+	{DRUM_PITCH_RIDE_1, 8, MODIFIER_SHARP},
+	{DRUM_PITCH_RIDE_2, 8, MODIFIER_SHARP},
+	{DRUM_PITCH_BELL_RIDE, 8, MODIFIER_FLAT},
+	{DRUM_PITCH_CRASH_1, 9, MODIFIER_SHARP},
+	{DRUM_PITCH_CRASH_2, 9, MODIFIER_SHARP},
+	{DRUM_PITCH_CHINESE, 9, MODIFIER_FLAT},
+};
+
+/*
+		DRUM_PITCH_SIDE_STICK = 37,
+		DRUM_PITCH_CLAP = 39,
+		DRUM_PITCH_TAMBOURINE = 54,
+		DRUM_PITCH_SPLASH = 55,
+		DRUM_PITCH_COWBELL = 56,
+		DRUM_PITCH_VIBRASLASH = 58,
+		DRUM_PITCH_BONGO_HI = 60,
+		DRUM_PITCH_BONGO_LOW = 61,
+*/
+
+
 Clef::Clef(int _type, const string &_symbol, int _offset)
 {
 	type = _type;
@@ -26,46 +69,14 @@ Clef::Clef(int _type, const string &_symbol, int _offset)
 int Clef::pitch_to_position(int pitch, const Scale &s, int &modifier) const
 {
 	if (type == TYPE_DRUMS){
-		modifier = MODIFIER_NONE;
-		/*if (pitch == 35)	return "bass      (akk)";
-		if (pitch == 36)	return "bass";
-		if (pitch == 37)	return "side stick";
-		if (pitch == 38)	return "snare";
-		if (pitch == 39)	return "clap";
-		if (pitch == 40)	return "snare     (electronic)";
-		if (pitch == 41)	return "tom - floor low";
-		if (pitch == 42)	return "hihat - closed";
-		if (pitch == 43)	return "tom - floor hi";
-		if (pitch == 44)	return "hihat - pedal";
-		if (pitch == 45)	return "tom - low";
-		if (pitch == 46)	return "hihat - open";
-		if (pitch == 47)	return "tom - low mid";
-		if (pitch == 48)	return "tom - hi mid";
-		if (pitch == 49)	return "crash 1";
-		if (pitch == 50)	return "tom - hi";
-		if (pitch == 51)	return "ride 1";
-		if (pitch == 52)	return "chinese";
-		if (pitch == 53)	return "bell ride";
-		if (pitch == 54)	return "tambourine";
-		if (pitch == 55)	return "splash";
-		if (pitch == 56)	return "cowbell";
-		if (pitch == 57)	return "crash 2";
-		if (pitch == 58)	return "vibraslash?";
-		if (pitch == 59)	return "ride 2";
-		if (pitch == 60)	return "bongo - hi";
-		if (pitch == 61)	return "bongo - low";*/
-		if ((pitch == 35) or (pitch == 36)) // bass
-			return 1;
-		if ((pitch == 38) or (pitch == 40)) // snare
-			return 5;
-		if ((pitch == 48) or (pitch == 50)) // tom hi
-			return 9;
-		if ((pitch == 45) or (pitch == 47)) // tom mid
-			return 8;
-		if ((pitch == 41) or (pitch == 43)) // tom floor
-			return 3;
+		for (int i=0; i<NUM_DRUM_CLEF_POSITIONS; i++)
+			if (pitch == DrumClefPositions[i].pitch){
+				modifier = DrumClefPositions[i].modifier;
+				return DrumClefPositions[i].clef_pos;
+			}
+
 		modifier = MODIFIER_SHARP;
-		return 0;
+		return 13;
 	}
 	int octave = pitch_get_octave(pitch);
 	int rel = pitch_to_rel(pitch);
@@ -79,5 +90,11 @@ int Clef::pitch_to_position(int pitch, const Scale &s, int &modifier) const
 
 int Clef::position_to_pitch(int pos, const Scale &s, int mod) const
 {
+	if (type == TYPE_DRUMS){
+		for (int i=0; i<NUM_DRUM_CLEF_POSITIONS; i++)
+			if ((pos == DrumClefPositions[i].clef_pos) and (mod == DrumClefPositions[i].modifier))
+				return DrumClefPositions[i].pitch;
+		return 70;//DRUM_PITCH_BASS;
+	}
 	return s.transform_out(pos + offset, mod);
 }

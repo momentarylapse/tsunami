36724ef41161c6f2a5fdb0b780fe621c1b393a07
Michael Ankele <michi@lupina.de>
2018-03-18 21:46:03 +0100

    SymbolRenderer compression

--- src/View/AudioViewTrack.cpp
+++ src/View/AudioViewTrack.cpp
@@ -34,22 +34,58 @@ class SymbolRenderer
 		float size;
 		string text;
 		Image im;
+
+		Symbol(float _size, const string &_text)
+		{
+			text = _text;
+			size = _size;
+			im.create(size * text.num, size * 1.5f, color(0,0,0,0));
+		}
+
+		void render()
+		{
+			// render symbol into image
+			Painter *p = hui::start_image_paint(im);
+			p->setColor(White);
+			p->setFontSize(size);
+			p->drawStr(0, 0, text);
+			hui::end_image_paint(im, p);
+		}
+
+		void compress()
+		{
+			// used area?
+			int xmax = 0, ymax = 0;
+			for (int x=0; x<im.width; x++)
+				for (int y=0; y<im.height; y++)
+					if (im.getPixel(x, y).r > 0){
+						if (x > xmax)
+							xmax = x;
+						if (y > ymax)
+							ymax = y;
+					}
+
+			// how much unused area?
+			if (im.width * im.height - xmax * ymax < 15)
+				return;
+
+			// copy used part into smaller image
+			//printf("compress %d %d -> %d %d\n", im.width, im.height, xmax, ymax);
+			Image _im = im;
+			im.create(xmax, ymax, Black);
+			for (int x=0; x<xmax; x++)
+				for (int y=0; y<ymax; y++)
+					im.setPixel(x, y, _im.getPixel(x,y));
+		}
 	};
 	static Array<Symbol*> symbols;
 
 	static Symbol *make_symbol(float size, const string &s)
 	{
-		msg_write("new symbol " + f2s(size, 1) + " " + s);
-		Symbol *sym = new Symbol;
-		sym->im.create(size * s.num, size * 1.5f, color(0,0,0,0));
-		sym->text = s;
-		sym->size = size;
-
-		Painter *p = hui::start_image_paint(sym->im);
-		p->setColor(White);
-		p->setFontSize(size);
-		p->drawStr(0, 0, s);
-		hui::end_image_paint(sym->im, p);
+		//msg_write("new symbol " + f2s(size, 1) + " " + s);
+		Symbol *sym = new Symbol(size, s);
+		sym->render();
+		sym->compress();
 
 		symbols.add(sym);
 		return sym;
@@ -57,9 +93,12 @@ class SymbolRenderer
 
 	static Symbol *get_symbol(float size, const string &s)
 	{
+		// already exists?
 		for (Symbol *sym: symbols)
-			if (sym->text == s and fabs(sym->size - size) <= 1)
+			if (sym->text == s and fabs(sym->size - size) <= 0.5f)
 				return sym;
+
+		// no -> make new
 		return make_symbol(size, s);
 	}
 public:

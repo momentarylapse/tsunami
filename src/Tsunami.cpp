0584bd3f03f1ec4f8bbc645e26b76577a22d18c7
Michael Ankele <michi@lupina.de>
2018-08-09 21:09:30 +0200

    v0.7.13

--- src/Tsunami.cpp
+++ src/Tsunami.cpp
@@ -28,7 +28,7 @@
 
 
 const string AppName = "Tsunami";
-const string AppVersion = "0.7.12.2";
+const string AppVersion = "0.7.13.0";
 const string AppNickname = "absolute 2er0";
 
 Tsunami *tsunami = nullptr;

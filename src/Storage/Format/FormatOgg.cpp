5664253552147222c492fa4e190adc51337ddc86
Michael Ankele <michi@lupina.de>
2019-11-10 20:07:59 +0100

    fix: ogg saving cut-off

--- src/Storage/Format/FormatOgg.cpp
+++ src/Storage/Format/FormatOgg.cpp
@@ -49,8 +49,7 @@ FormatDescriptorOgg::FormatDescriptorOgg() :
 
 
 
-int oe_write_page(ogg_page *page, FILE *fp)
-{
+int oe_write_page(ogg_page *page, FILE *fp) {
     int written;
     written = fwrite(page->header,1,page->header_len, fp);
     written += fwrite(page->body,1,page->body_len, fp);
@@ -59,14 +58,13 @@ int oe_write_page(ogg_page *page, FILE *fp)
 }
 
 
-void FormatOgg::save_via_renderer(StorageOperationData *od)
-{
+void FormatOgg::save_via_renderer(StorageOperationData *od) {
 	Port *r = od->renderer;
 
 	float OggQuality = hui::Config.get_float("OggQuality", 0.5f);
 
 	FILE *f = fopen(od->filename.c_str(), "wb");
-	if (!f){
+	if (!f) {
 		od->error("can not create file");
 		return;
 	}
@@ -74,8 +72,8 @@ void FormatOgg::save_via_renderer(StorageOperationData *od)
 	vorbis_info vi;
 	vorbis_info_init(&vi);
 	int channels = od->channels_suggested;
-	if (vorbis_encode_setup_vbr(&vi, channels, od->session->sample_rate(), OggQuality)){
-		od->error("vorbis_encode_setup_vbr");
+	if (vorbis_encode_setup_vbr(&vi, channels, od->session->sample_rate(), OggQuality)) {
+		od->error("vorbis_encode_setup_vbr() failed");
 		return;
 	}
 	vorbis_encode_setup_init(&vi); // ?
@@ -83,22 +81,22 @@ void FormatOgg::save_via_renderer(StorageOperationData *od)
 	vorbis_dsp_state vd;
 	vorbis_block vb;
 	ogg_stream_state os;
-	if (vorbis_analysis_init(&vd, &vi)){
-		od->error("vorbis_analysis_init");
+	if (vorbis_analysis_init(&vd, &vi)) {
+		od->error("vorbis_analysis_init() failed");
 		return;
 	}
-	if (vorbis_block_init(&vd, &vb)){
-		od->error("vorbis_block_init");
+	if (vorbis_block_init(&vd, &vb)) {
+		od->error("vorbis_block_init() failed");
 		return;
 	}
-	if (ogg_stream_init(&os, 0)){
-		od->error("ogg_stream_init");
+	if (ogg_stream_init(&os, 0)) {
+		od->error("ogg_stream_init() failed");
 		return;
 	}
 
 	vorbis_comment vc;
 	vorbis_comment_init(&vc);
-	for (Tag &tag : od->tags)
+	for (Tag &tag: od->tags)
 		vorbis_comment_add_tag(&vc, tag.key.c_str(), tag.value.c_str());
 	ogg_packet header_main;
 	ogg_packet header_comments;
@@ -111,11 +109,11 @@ void FormatOgg::save_via_renderer(StorageOperationData *od)
 	ogg_packet op;
 
 	int result;
-	while((result = ogg_stream_flush(&os, &og))){
+	while((result = ogg_stream_flush(&os, &og))) {
 		if (!result)
 			break;
 		int ret = oe_write_page(&og, f);
-		if (ret != og.header_len + og.body_len){
+		if (ret != og.header_len + og.body_len) {
 			/*opt->error(_("Failed writing header to output stream\n"));
 			ret = 1;
 			goto cleanup;*/
@@ -128,67 +126,57 @@ void FormatOgg::save_via_renderer(StorageOperationData *od)
 	//int eos = 0;
 	int written = 0;
 	int samples = od->num_samples;
-#define READSIZE		1<<12
+	const int READSIZE = 1<<12;
 	int nn = 0;
 
 	AudioBuffer buf(READSIZE, channels);
 
 	int eos = 0;
-	while(!eos){
-		//msg_write(written);
+	while(!eos) {
 
+		// renderer -> vorbis
 		int samples_read = r->read_audio(buf);
-		if (samples_read <= 0)
-			break;
+		if (samples_read > 0) {
 
-		float **buffer = vorbis_analysis_buffer(&vd, samples_read);
-		for (int c=0; c<channels; c++)
-			for (int i=0;i<samples_read;i++)
-				buffer[c][i] = buf.c[c][i];
+			float **buffer = vorbis_analysis_buffer(&vd, samples_read);
+			for (int c=0; c<channels; c++)
+				for (int i=0; i<samples_read; i++)
+					buffer[c][i] = buf.c[c][i];
 
-		written += samples_read;
-		vorbis_analysis_wrote(&vd, samples_read);
+			written += samples_read;
+			vorbis_analysis_wrote(&vd, samples_read);
+		} else {
+			// end of renderer stream
+			vorbis_analysis_wrote(&vd, 0);
+		}
 
 
 		nn ++;
-		if (nn > 8){
+		if (nn > 8) {
 			od->set(float(written) / (float)samples);
 			nn = 0;
 		}
 
-        /* While we can get enough data from the library to analyse, one
-           block at a time... */
-        while (vorbis_analysis_blockout(&vd,&vb) == 1){
-				//msg_write("b");
+        while (vorbis_analysis_blockout(&vd, &vb) == 1) {
 
-            /* Do the main analysis, creating a packet */
             vorbis_analysis(&vb, nullptr);
             vorbis_bitrate_addblock(&vb);
 
-            while (vorbis_bitrate_flushpacket(&vd, &op)){
-				//msg_write("f");
-                /* Add packet to bitstream */
-                ogg_stream_packetin(&os,&op);
-                //packetsdone++;
-
-                /* If we've gone over a page boundary, we can do actual output,
-                   so do so (for however many pages are available) */
+            while (vorbis_bitrate_flushpacket(&vd, &op)) {
+                ogg_stream_packetin(&os, &op);
 
 				//eos = 0;
-                while( !eos){
-				//msg_write("p");
-                    int result = ogg_stream_pageout(&os,&og);
+                while (!eos) {
+                    int result = ogg_stream_pageout(&os, &og);
                     if (!result)
 						break;
 
                     int ret = oe_write_page(&og, f);
-                    if (ret != og.header_len + og.body_len){
-                        od->error("Failed writing data to output stream");
+                    if (ret != og.header_len + og.body_len) {
+                        od->error("failed writing data to output stream");
                         ret = 1;
                         return;
                     }
-                    //else
-                      //  bytes_written += ret;
 
                     if (ogg_page_eos(&og))
                         eos = 1;
@@ -206,11 +194,10 @@ void FormatOgg::save_via_renderer(StorageOperationData *od)
 }
 
 
-void FormatOgg::load_track(StorageOperationData *od)
-{
+void FormatOgg::load_track(StorageOperationData *od) {
 	Track *t = od->track;
 
-	if (ov_fopen((char*)od->filename.c_str(), &vf)){
+	if (ov_fopen((char*)od->filename.c_str(), &vf)) {
 		od->error("can not open file");
 		return;
 	}
@@ -218,7 +205,7 @@ void FormatOgg::load_track(StorageOperationData *od)
 	//int bits = 16;
 	int channels = 2;
 	int freq = DEFAULT_SAMPLE_RATE;
-	if (vi){
+	if (vi) {
 		channels = vi->channels;
 		freq = vi->rate;
 	}
@@ -228,7 +215,7 @@ void FormatOgg::load_track(StorageOperationData *od)
 	// tags
 	t->song->tags.clear();
 	char **ptr = ov_comment(&vf,-1)->user_comments;
-	while (*ptr){
+	while (*ptr) {
 		string s = *ptr;
 		int offset = s.find("=");
 		if (offset > 0)
@@ -237,7 +224,7 @@ void FormatOgg::load_track(StorageOperationData *od)
     }
 
 	int samples = (int)ov_pcm_total(&vf, -1);
-	if (od->only_load_metadata){
+	if (od->only_load_metadata) {
 		ov_clear(&vf);
 		od->layer->add_marker(Range(0, samples), "dummy");
 		return;
@@ -248,18 +235,18 @@ void FormatOgg::load_track(StorageOperationData *od)
 	int read = 0;
 	int nn = 0;
 
-	while (true){
+	while (true) {
 
 		int chunk_read = 0;
 		bool error = false;
-		while (true){
+		while (true) {
 			int toread = CHUNK_SIZE - chunk_read;
 			int r = ov_read(&vf, &data[chunk_read], toread, 0, 2, 1, &current_section); // 0,2,1 = little endian, 16bit, signed
 			//msg_write(r);
 
-			if (r == 0){
+			if (r == 0) {
 				break;
-			}else if (r < 0){
+			} else if (r < 0) {
 				od->error("ov_read failed");
 				error = true;
 				break;
@@ -278,7 +265,7 @@ void FormatOgg::load_track(StorageOperationData *od)
 		import_data(od->layer, data, channels, SampleFormat::SAMPLE_FORMAT_16, dsamples, _offset);
 		read += chunk_read;
 		nn ++;
-		if (nn > 8){
+		if (nn > 8) {
 			od->set((float)read / (float)(samples * 4));
 			nn = 0;
 		}

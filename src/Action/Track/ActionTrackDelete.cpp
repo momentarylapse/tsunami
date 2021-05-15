4d20fca55d27e067e56ed5b197d6d46293eadf6c
Michael Ankele <michi@lupina.de>
2021-05-16 00:43:43 +0200

    delete master -> ungroup

--- src/Action/Track/ActionTrackDelete.cpp
+++ src/Action/Track/ActionTrackDelete.cpp
@@ -10,6 +10,8 @@
 #include "ActionTrack__DeleteEmpty.h"
 #include <assert.h>
 #include "Buffer/ActionTrack__DeleteBuffer.h"
+#include "Data/ActionTrackSetTarget.h"
+#include "../../Data/Song.h"
 #include "../../Data/Track.h"
 #include "../../Data/TrackLayer.h"
 #include "../../Data/SampleRef.h"
@@ -29,6 +31,10 @@ void ActionTrackDelete::build(Data *d) {
 		for (int i=l->samples.num-1; i>=0; i--)
 			add_sub_action(new ActionTrackDeleteSample(l->samples[i]), d);
 	}
+
+	for (auto t: weak(track->song->tracks))
+		if (t->send_target == track)
+			add_sub_action(new ActionTrackSetTarget(t, nullptr), d);
 
 	// delete the track itself
 	add_sub_action(new ActionTrack__DeleteEmpty(track), d);

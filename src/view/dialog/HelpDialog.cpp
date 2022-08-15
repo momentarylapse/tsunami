e3aeaa1596b28d8466715efee135c9fbfcc4c887
Michael Ankele <michi@lupina.de>
2022-08-15 18:16:35 +0200

    [gui] prettier HelpDialog

--- src/view/dialog/HelpDialog.cpp
+++ src/view/dialog/HelpDialog.cpp
@@ -10,8 +10,22 @@
 HelpDialog::HelpDialog(hui::Window *_parent) :
 	hui::Dialog("help-dialog", _parent)
 {
+	auto select_page = [this] (int index) {
+		set_int("tab", index);
+		check("show-page-0", index == 0);
+		check("show-page-1", index == 1);
+		check("show-page-2", index == 2);
+		check("show-page-3", index == 3);
+	};
+	select_page(0);
+
 	if (hui::config.get_bool("FirstStart", true))
 		hide_control("first-time-message", false);
-	event("close", [=]{ request_destroy(); });
+
+	event("close", [this] { request_destroy(); });
+	event("show-page-0", [select_page] { select_page(0); });
+	event("show-page-1", [select_page] { select_page(1); });
+	event("show-page-2", [select_page] { select_page(2); });
+	event("show-page-3", [select_page] { select_page(3); });
 }
 

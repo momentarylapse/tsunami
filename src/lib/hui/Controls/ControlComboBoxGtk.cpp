b360543d957344d5993438a0396d262ac24698d3
Michael Ankele <michi@lupina.de>
2023-03-19 23:39:57 +0100

    [hui] switch gtk_combo_box to gtk_drop_down

--- src/lib/hui/Controls/ControlComboBoxGtk.cpp
+++ src/lib/hui/Controls/ControlComboBoxGtk.cpp
@@ -13,14 +13,26 @@
 namespace hui
 {
 
-void on_gtk_combo_box_change(GtkWidget *widget, gpointer data)
-{	reinterpret_cast<Control*>(data)->notify(EventID::CHANGE);	}
+#if GTK_CHECK_VERSION(4,10,0)
+static void on_gtk_drop_down_activate(GtkDropDown *self, void *p, Control *data) {
+	data->notify(EventID::CHANGE);
+}
+#else
+static void on_gtk_combo_box_change(GtkWidget *widget, Control *data) {
+	data->notify(EventID::CHANGE);
+}
+#endif
 
 ControlComboBox::ControlComboBox(const string &title, const string &id) :
 	Control(CONTROL_COMBOBOX, id)
 {
 	auto parts = split_title(title);
 	editable = option_has(get_option_from_title(title), "editable");
+#if GTK_CHECK_VERSION(4,10,0)
+	auto model = gtk_string_list_new(nullptr);
+	widget = gtk_drop_down_new(G_LIST_MODEL(model), nullptr);
+#else
+
 	if (editable) {
 		widget = gtk_combo_box_text_new_with_entry();
 #if GTK_CHECK_VERSION(4,0,0)
@@ -31,26 +43,39 @@ ControlComboBox::ControlComboBox(const string &title, const string &id) :
 	} else {
 		widget = gtk_combo_box_text_new();
 	}
-	take_gtk_ownership();
-	g_signal_connect(G_OBJECT(widget), "changed", G_CALLBACK(&on_gtk_combo_box_change), this);
+#endif
 
 	if ((parts.num > 1) or (parts[0] != ""))
 		for (string &p: parts)
 			__add_string(p);
 
 	set_int(0);
+
+#if GTK_CHECK_VERSION(4,10,0)
+	take_gtk_ownership();
+	g_signal_connect(G_OBJECT(widget), "notify::selected", G_CALLBACK(&on_gtk_drop_down_activate), this);
+#else
+	take_gtk_ownership();
+	g_signal_connect(G_OBJECT(widget), "changed", G_CALLBACK(&on_gtk_combo_box_change), this);
+#endif
 }
 
 string ControlComboBox::get_string() {
+#if GTK_CHECK_VERSION(4,10,0)
+	auto o = gtk_drop_down_get_selected_item(GTK_DROP_DOWN(widget));
+	return gtk_string_object_get_string(GTK_STRING_OBJECT(o)); //g_value_get_string(G_VALUE(o));
+#else
 	char *c = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(widget));
 	string s = c;
 	g_free(c);
 	return s;
+#endif
 }
 
 void ControlComboBox::__set_string(const string &str) {
 	if (editable) {
-#if GTK_CHECK_VERSION(4,0,0)
+#if GTK_CHECK_VERSION(4,10,0)
+#elif GTK_CHECK_VERSION(4,0,0)
 		gtk_editable_set_text(GTK_EDITABLE(gtk_combo_box_get_child(GTK_COMBO_BOX(widget))), sys_str(str));
 #else
 		gtk_entry_set_text(GTK_ENTRY(gtk_bin_get_child(GTK_BIN(widget))), sys_str(str));
@@ -62,7 +87,10 @@ void ControlComboBox::__set_string(const string &str) {
 }
 
 void ControlComboBox::__add_string(const string& str) {
-#if GTK_CHECK_VERSION(3,0,0)
+#if GTK_CHECK_VERSION(4,10,0)
+	auto m = gtk_drop_down_get_model(GTK_DROP_DOWN(widget));
+	gtk_string_list_append(GTK_STRING_LIST(m), sys_str(str));
+#elif GTK_CHECK_VERSION(3,0,0)
 	gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(widget), NULL, sys_str(str));
 #else
 	gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(widget), sys_str(str));
@@ -70,16 +98,26 @@ void ControlComboBox::__add_string(const string& str) {
 }
 
 void ControlComboBox::__set_int(int i) {
+#if GTK_CHECK_VERSION(4,10,0)
+	gtk_drop_down_set_selected(GTK_DROP_DOWN(widget), i);
+#else
 	gtk_combo_box_set_active(GTK_COMBO_BOX(widget), i);
+#endif
 }
 
 int ControlComboBox::get_int() {
+#if GTK_CHECK_VERSION(4,10,0)
+	return gtk_drop_down_get_selected(GTK_DROP_DOWN(widget));
+#else
 	return gtk_combo_box_get_active(GTK_COMBO_BOX(widget));
+#endif
 }
 
-void ControlComboBox::__reset()
-{
-#if GTK_CHECK_VERSION(3,0,0)
+void ControlComboBox::__reset() {
+#if GTK_CHECK_VERSION(4,10,0)
+	auto m = gtk_drop_down_get_model(GTK_DROP_DOWN(widget));
+	g_list_store_remove_all(G_LIST_STORE(m));
+#elif GTK_CHECK_VERSION(3,0,0)
 	gtk_combo_box_text_remove_all(GTK_COMBO_BOX_TEXT(widget));
 #else
 	/*GtkTreeModel *m = gtk_combo_box_get_model(GTK_COMBO_BOX(widget));

f76009197b4e9cf60b7fa868a74c9aa4735b65c6
Michael Ankele <michi@lupina.de>
2023-05-21 23:35:20 +0200

    [gui] QuestionDialogFloat added with "mode=volume" option

--- src/view/dialog/QuestionDialog.cpp
+++ src/view/dialog/QuestionDialog.cpp
@@ -7,10 +7,13 @@
 
 #include "QuestionDialog.h"
 #include "../../lib/base/pointer.h"
+#include "../../data/base.h"
+#include <math.h>
 
 
 bool QuestionDialogInt::aborted;
 bool QuestionDialogIntInt::aborted;
+bool QuestionDialogFloat::aborted;
 bool QuestionDialogString::aborted;
 
 QuestionDialogInt::QuestionDialogInt(hui::Window *_parent, const string &question, const string &options, std::function<void(int)> _cb)
@@ -73,6 +76,127 @@ QuestionDialogIntInt::QuestionDialogIntInt(hui::Window *_parent, const string &q
 
 void QuestionDialogIntInt::ask(hui::Window *parent, const string &question, const Array<string> &labels, const Array<string> &options, std::function<void(int,int)> cb) {
 	hui::fly(new QuestionDialogIntInt(parent, question, labels, options, cb));
+}
+
+
+
+QuestionDialogFloat::QuestionDialogFloat(hui::Window *_parent, const string &question, float value0, float min, float max, const string &options, Callback _cb)
+: hui::Dialog("question-dialog-float", _parent) {
+	cb = _cb;
+	result = value0;
+	aborted = true;
+	_min = min;
+	_max = max;
+	_min_db = amplitude2db(::max(_min, 0.001f));
+	_max_db = amplitude2db(_max);
+	//set_string("question", question);
+	set_title(question);
+	if (options.find("volume") >= 0) {
+		set_mode(Mode::VOLUME_PERCENT);
+	} else {
+		set_mode(Mode::DIRECT);
+	}
+
+	event("value", [this] {
+		result = get_spin();
+		set_slider(result);
+	});
+	event("slider", [this] {
+		result = get_slider();
+		set_spin(result);
+	});
+	event("unit", [this] {
+		if (mode == Mode::VOLUME_PERCENT)
+			set_mode(Mode::VOLUME_DB);
+		else if (mode == Mode::VOLUME_DB)
+			set_mode(Mode::VOLUME_PERCENT);
+	});
+	event("cancel", [this] {
+		cb(-1);
+		request_destroy();
+	});
+	event("ok", [this] {
+		aborted = false;
+		cb(result);
+		request_destroy();
+	});
+}
+
+float QuestionDialogFloat::db2slider(float db) {
+	return (atan(db / TAN_SCALE) - atan(DB_MIN / TAN_SCALE)) / (atan(DB_MAX / TAN_SCALE) - atan(DB_MIN / TAN_SCALE));
+}
+
+float QuestionDialogFloat::slider2db(float val) {
+	return tan(atan(DB_MIN / TAN_SCALE) + val * (atan(DB_MAX / TAN_SCALE)- atan(DB_MIN / TAN_SCALE))) * TAN_SCALE;
+}
+
+void QuestionDialogFloat::set_mode(Mode m) {
+	mode = m;
+	reset("slider");
+	if (mode == Mode::VOLUME_PERCENT) {
+		set_options("value", format("range=%f:%f:%f", _min * 100, _max * 100, 0.1f));
+		hide_control("unit", false);
+		set_string("unit", "%");
+		add_string("slider", format("%f\\<span size='x-small'>%d</span>", 0.0f, 0));
+		add_string("slider", format("%f\\<span size='x-small'>%d</span>", 0.25f, 50));
+		add_string("slider", format("%f\\<span size='x-small'>%d</span>", 0.5f, 100));
+		add_string("slider", format("%f\\<span size='x-small'>%d</span>", 0.75f, 150));
+		add_string("slider", format("%f\\<span size='x-small'>%d</span>", 1.0f, 200));
+	} else if (mode == Mode::VOLUME_DB) {
+		set_options("value", format("range=%f:%f:%f", _min_db, _max_db, 0.1f));
+		hide_control("unit", false);
+		set_string("unit", "dB");
+		add_string("slider", format("%f\\<span size='x-small'>%+d</span>", db2slider(DB_MAX), (int)DB_MAX));
+		add_string("slider", format("%f\\", db2slider(6), 6));
+		add_string("slider", format("%f\\<span size='x-small'>%d</span>", db2slider(0), 0));
+		add_string("slider", format("%f\\", db2slider(-6), -6));
+		add_string("slider", format("%f\\<span size='x-small'>%d</span>", db2slider(-12), -12));
+		add_string("slider", format("%f\\<span size='x-small'>%d</span>", db2slider(-24), -24));
+		add_string("slider", format(u8"%f\\<span size='x-small'>-\u221e</span>", db2slider(DB_MIN))); // \u221e
+	} else if (mode == Mode::DIRECT) {
+		set_options("value", format("range=%f:%f:%f", _min, _max, 0.01f));
+		hide_control("unit", true);
+	}
+	set_spin(result);
+	set_slider(result);
+}
+
+void QuestionDialogFloat::set_spin(float f) {
+	if (mode == Mode::VOLUME_PERCENT)
+		set_float("value", f * 100);
+	else if (mode == Mode::VOLUME_DB)
+		set_float("value", amplitude2db(f));
+	else if (mode == Mode::DIRECT)
+		set_float("value", f);
+}
+
+void QuestionDialogFloat::set_slider(float f) {
+	if (mode == Mode::VOLUME_DB)
+		set_float("slider", db2slider(amplitude2db(f)));
+	else if (mode == Mode::VOLUME_PERCENT)
+		set_float("slider", f / 2);
+	else
+		set_float("slider", (f - _min) / (_max - _min));
+}
+
+float QuestionDialogFloat::get_spin() {
+	if (mode == Mode::VOLUME_PERCENT)
+		return get_float("value") / 100;
+	if (mode == Mode::VOLUME_DB)
+		return db2amplitude(get_float("value"));
+	return get_float("value");
+}
+
+float QuestionDialogFloat::get_slider() {
+	if (mode == Mode::VOLUME_DB)
+		return db2amplitude(slider2db(get_float("slider")));
+	if (mode == Mode::VOLUME_PERCENT)
+		return get_float("slider") * 2;
+	return _min + get_float("slider") * (_max - _min);
+}
+
+void QuestionDialogFloat::ask(hui::Window *parent, const string &question, Callback cb, float value0, float min, float max, const string &options) {
+	hui::fly(new QuestionDialogFloat(parent, question, value0, min, max, options, cb));
 }
 
 

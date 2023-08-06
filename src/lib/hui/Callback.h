8453815ca9f7a8a5c943377d4d97deb86debc8e8
Michael Ankele <michi@lupina.de>
2023-08-06 11:18:06 +0200

    [hui] future/promise interface

--- src/lib/hui/Callback.h
+++ src/lib/hui/Callback.h
@@ -9,6 +9,7 @@
 #define SRC_LIB_HUI_CALLBACK_H_
 
 #include <functional>
+#include "../base/pointer.h"
 
 class Painter;
 
@@ -24,11 +25,62 @@ typedef void kaba_member_callback_p(EventHandler *h, void *p);
 typedef std::function<void()> Callback;
 typedef std::function<void(Painter*)> CallbackP;
 
+template<class T>
+struct future;
+
+// internal
+template<class T>
+struct promise : public Sharable<base::Empty> {
+	using Callback = std::function<void(T)>;
+	using CallbackFail = std::function<void()>;
+
+	Callback cb_success;
+	CallbackFail cb_fail;
+
+	void set_value(T t) {
+		if (cb_success)
+			cb_success(t);
+	}
+	void fail() {
+		if (cb_fail)
+			cb_fail();
+	}
+	future<T> get_future() {
+		return future<T>(*this);
+	}
+};
+
+// returned by async function
+// this is currently only a thin interface to set callbacks
+template<class T>
+struct future {
+	using P = promise<T>;
+	using Callback = std::function<void(T)>;
+	using CallbackFail = std::function<void()>;
+
+	P& _promise;
+
+	future(P& p) : _promise(p) {
+	}
+	future(future<T>& f) : _promise(f._promise) {
+	}
+
+	future<T>& on(Callback cb) {
+		_promise.cb_success = cb;
+		return *this;
+	}
+
+	future<T>& on_fail(CallbackFail cb) {
+		_promise.cb_fail = cb;
+		return *this;
+	}
+};
 
 
 void set_idle_function(const Callback &idle_function);
 int run_later(float time, const Callback &function);
 int run_repeated(float time, const Callback &function);
+int run_in_gui_thread(const Callback &function);
 void cancel_runner(int i);
 
 

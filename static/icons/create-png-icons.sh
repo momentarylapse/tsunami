ddeeb8a9e342d4dbfca0c7c364a3aca0f7dad13e
Michael Ankele <michi@lupina.de>
2024-12-01 21:20:58 +0100

    [icons] document-properties added

--- static/icons/create-png-icons.sh
+++ static/icons/create-png-icons.sh
@@ -1,6 +1,7 @@
+
 for f in *-symbolic.svg
 do
-	gtk-encode-symbolic-svg -o hicolor/32x32/actions $f 32x32
-	gtk-encode-symbolic-svg -o hicolor/48x48/actions $f 48x48
-	gtk-encode-symbolic-svg -o hicolor/64x64/actions $f 64x64
+	gtk4-encode-symbolic-svg -o hicolor/32x32/actions $f 32x32
+	gtk4-encode-symbolic-svg -o hicolor/48x48/actions $f 48x48
+	gtk4-encode-symbolic-svg -o hicolor/64x64/actions $f 64x64
 done

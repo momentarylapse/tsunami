78048d0168121d4dff926796e7372f64c6a62fab
Michael Ankele <michi@lupina.de>
2026-06-30 14:41:07 +0200

    [lib/pdf] extracted from lib/doc

--- src/storage/format/FormatPdf.cpp
+++ src/storage/format/FormatPdf.cpp
@@ -11,14 +11,14 @@
 #include "../../data/Song.h"
 #include "../../data/Track.h"
 #include "../../data/SongSelection.h"
-#include "../../lib/doc/pdf.h"
+#include "../../lib/pdf/pdf.h"
 #include "../../lib/hui/language.h"
 #include "../../lib/math/rect.h"
 #include "../../view/ColorScheme.h"
 #include "../../view/helper/SymbolRenderer.h"
 #include "../../view/HoverData.h"
 #include "../../view/painter/MultiLinePainter.h"
-#include <math.h>
+#include <cmath>
 
 namespace tsunami {
 
